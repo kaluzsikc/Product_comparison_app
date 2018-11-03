7c5b53bf0cf3e2ffc79b1c63b2d6f61c43dc4c97
kaluzsikc <kaluzsikc@gmail.com>
2018-11-03 20:51:40 +0000

    Added structure for Results page

--- src/containers/ResultBox.js
+++ src/containers/ResultBox.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 const Results = () => (
-    <div>
-        <h4>Result Box</h4>
+    <div className='resultPage'>
+        <h1>Result Box</h1>
+        <div className="productSearch">
+            <h4>Search products: </h4>
+        </div>
+        <div className='productInfo'>
+            <h4>Product Info Box</h4>
+        </div>
+        <div className='resultList'>
+            <h4>Result List:</h4>
+        </div>
+
+        {/* <div className='productSuggetion'>
+
+        </div> */}
     </div>
 );
 
