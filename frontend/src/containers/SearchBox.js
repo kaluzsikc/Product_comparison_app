1043de37ed39be2c638ab9d23a6c4b259b100eaf
kaluzsikc <kaluzsikc@gmail.com>
2018-11-04 13:22:31 +0000

    Added created and added files in frontend folder

---
+++ frontend/src/containers/SearchBox.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+
+class SearchBox extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            query: '',
+            results: []
+        }
+
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    getInfo = () => {
+        //fetch request?
+    }
+
+    handleInput(event) {
+        this.setState({ text: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('form submitted');
+        const text = this.state.text.trim();
+        if (!text) {
+            return;
+        }
+        // const newSearch = { text: text };
+
+        console.log('text:',text);
+        this.setState({ text: '' })
+    }
+
+//     handleChange(event) {
+//         const onCurrencySelected = event.target.value;
+// }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Search product..."
+                        value={this.state.text}
+                        onChange={this.handleInput}
+                    />
+                    <select id='currency-seelctor' defaultValue="default">
+                        <option disabled value="default">Select currency</option>
+                        <option value="GBP">£ - GBP</option>
+                        <option value="USD">$ - USD</option>
+                        <option value="EURO">€ - EURO</option>
+                        <option value="Yen">¥ - JPY</option>
+                        <option value="Yuan">¥ - CNY</option>
+                        <option value="Real">R$ - BRL</option>
+                        <option value="Rupee">₹ - INR</option>
+                        <option value="SinDollar">$ - SGD</option>
+                        <option value="Lira">₺ - TRY</option>
+                        <option value="Peso">Mex$ - MXN</option>
+                        <option value="AUDollar">$ - AUD</option>
+                    </select>
+                    <input type="submit" value="Search" />
+                    {/* <p>{this.state.query}</p> */}
+                </form>
+            
+                {/* <select id='currency-seelctor' defaultValue="default">
+                    <option disabled value="default">Select currency</option>
+                    <option value="GBP">Pound Sterling</option>
+                    <option value="USD">US Dolar</option>
+                    <option value="EURO">Euro</option>
+                    <option value="Yen">Japanese Yen</option>
+                    <option value="Yuan">Chinese Yuan</option>
+                    <option value="Real">Brazilian Real</option>
+                    <option value="Rupee">Indian Rupee</option>
+                    <option value="SinDollar">Singapore Dollar</option>
+                    <option value="Lira">Turkish Lira</option>
+                    <option value="Peso">Mexican Peso</option>
+                    <option value="AUDollar">Australian Dollar</option>
+                </select> */}
+            </div>
+        )
+    }
+}
+
+export default SearchBox;
