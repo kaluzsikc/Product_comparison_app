c096e6fad76411b5a7f8f6ab805c124151d82621
kaluzsikc <kaluzsikc@gmail.com>
2018-11-04 11:03:09 +0000

    Added search bar and button

--- src/containers/SearchBox.js
+++ src/containers/SearchBox.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBox = () => (
-    <div>
-        <h4>Search Box</h4>
-        
-    </div>
-);
+import Results from './ResultBox';
 
-export default SearchBox; 
+
+class SearchBox extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            query: '',
+            results: []
+        }
+
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    getInfo = () => {
+        //fetch request?
+    }
+
+    handleInput(event) {
+        this.setState({ text: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('form submitted');
+        const text = this.state.text.trim();
+        if (!text) {
+            return;
+        }
+        // const newSearch = { text: text };
+
+        //reset the form fields
+        console.log(text);
+        this.setState({ text: '' })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Search product..."
+                        value={this.state.text}
+                        onChange={this.handleInput}
+                    />
+                    <input type="submit" value="Search" />
+                    <p>{this.state.query}</p>
+                </form>
+            
+                
+            </div>
+        )
+    }
+}
+
+export default SearchBox;
